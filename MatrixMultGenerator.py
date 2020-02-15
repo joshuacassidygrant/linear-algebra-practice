25f51f4525babbec4f231c35ff47c942bf4c7cb7
joshuacassidygrant <jc.grant22@gmail.com>
2020-02-15 11:16:01 -0800

    Working prototype

--- MatrixMultGenerator.py
+++ MatrixMultGenerator.py
@@ -3,15 +3,12 @@ import numpy as np
 
 class MatrixMultGenerator:
 
-    vmin = -5
-    vmax = 5
+    vmin = -2
+    vmax = 3
 
     def generate(self, w, h, s, restrictToIntegers):
         m1 = np.random.randint(self.vmin, self.vmax, size=(w, s))
-        print(m1)
         m2 = np.random.randint(self.vmin, self.vmax, size=(s, h))
-        print(m2)
         sol = m1@m2
-        print(sol)
-
+        return (m1, m2, sol)
         # TODO: create rational version
--- lap.py
+++ lap.py
@@ -2,6 +2,7 @@ import sys
 import getopt
 import MatrixMultGenerator
 import numpy as np
+import time
 
 
 def main(argv):
@@ -12,23 +13,43 @@ def main(argv):
 
     num_questions = int(args[0])
     num_questions_answered = 0
+    num_questions_correct = 0
+    start_time = time.time()
 
     gen = MatrixMultGenerator.MatrixMultGenerator()
 
     while(num_questions > num_questions_answered):
         # TODO: generate these based on difficulty
-        rows = 2
-        cols = 3
-        mid = 4
-        gen.generate(rows, cols, mid, True)
+        rows = 3
+        cols = 1
+        mid = 3
+        m1, m2, sol = gen.generate(rows, cols, mid, True)
+        print(m1)
+        print(m2)
         try:
             in_entries = list(map(int, input("Input answer:").split()))
-            # TODO: make sure rows/cols match
-            in_mat = np.array(in_entries).reshape(rows, cols)
-            print(in_mat)
         except:
-            print("Input error")
+            print("Input error!")
+
+        if (len(in_entries) != rows * cols):
+            print("Incorrect entry count. Matrix shape is " +
+                  str(rows) + " x " + str(cols))
+            print(str(sol))
+        else:
+            in_mat = np.array(in_entries).reshape(rows, cols)
+            if (np.all(in_mat == sol)):
+                num_questions_correct += 1
+                print("CORRECT")
+            else:
+                print("WRONG: \n" + str(sol))
+
         num_questions_answered += 1
+
+    end_time = time.time()
+    total_time = end_time - start_time
+    accuracy = float(num_questions_correct) / float(num_questions_answered)
+    print("Accuracy: " + str(accuracy))
+    print("Time: " + str(total_time))
     return
 
 
